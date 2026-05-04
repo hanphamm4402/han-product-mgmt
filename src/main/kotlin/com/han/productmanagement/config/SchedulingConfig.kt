d354f3263da04611dbf2cb2d898cca18156646f3
hanphamm4402 <hanphamm.work@gmail.com>
2026-05-05 00:44:57 +0700

    add rest client builder bean

--- src/main/kotlin/com/han/productmanagement/config/SchedulingConfig.kt
+++ src/main/kotlin/com/han/productmanagement/config/SchedulingConfig.kt
@@ -7,5 +7,8 @@ import org.springframework.web.client.RestClient
 @Configuration
 class SchedulingConfig {
     @Bean
+    fun restClientBuilder(): RestClient.Builder = RestClient.builder()
+
+    @Bean
     fun restClient(builder: RestClient.Builder): RestClient = builder.build()
 }
